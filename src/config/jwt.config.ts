7f82e34b64ea23fb4204e16cf7ed14aa60d726e4
ioamra <iom.monchau@gmail.com>
2024-06-09 21:35:28 +0200

    prettier

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,11 +1,10 @@
 import jwt from "jsonwebtoken";
-import * as dotenv from 'dotenv';
-dotenv.config({path: './src/config/.env'});
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./src/config/.env" });
 
 export default class AuthenticationService {
-    
     private static readonly secretKey = process.env.JWT_PRIVATE_KEY;
-    
+
     static generateToken(payload: object, expiresIn: string): string {
         return jwt.sign(payload, this.secretKey, { expiresIn });
     }
@@ -14,10 +13,10 @@ export default class AuthenticationService {
         try {
             return jwt.verify(token, this.secretKey);
         } catch (error) {
-            if (error.name === 'TokenExpiredError') {
-                return 'Token expiré. Veuillez vous reconnecter.';
+            if (error.name === "TokenExpiredError") {
+                return "Token expiré. Veuillez vous reconnecter.";
             } else {
-                return 'Token invalide. Veuillez vous reconnecter.';
+                return "Token invalide. Veuillez vous reconnecter.";
             }
         }
     }
