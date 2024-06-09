e554264edbe9ebd54f5f952af5b789d14a4166c0
ioamra <iom.monchau@gmail.com>
2024-06-10 01:49:49 +0200

    maj db config

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,28 +1,18 @@
 import { Pool } from 'pg';
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./src/config/.env" });
 
 export const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: "postgres",
-    database: 'api_classic',
+    host: process.env.MODE === 'prod' ? process.env.DB_HOST : process.env.LOCAL_DB_HOST,
+    port: process.env.MODE === 'prod' ? parseInt(process.env.DB_PORT as string) : parseInt(process.env.LOCAL_DB_PORT as string),
+    user: process.env.MODE === 'prod' ? process.env.DB_USER : process.env.LOCAL_DB_USER,
+    password: process.env.MODE === 'prod' ? process.env.DB_PASSWORD : process.env.LOCAL_DB_PASSWORD,
+    database: process.env.MODE === 'prod' ? process.env.DB_NAME : process.env.LOCAL_DB_NAME,
     idleTimeoutMillis: 3000,
     connectionTimeoutMillis: 20000,
     max: 20000,
     allowExitOnIdle: true,
 });
-
-// export const pool = new Pool({
-//     host: process.env.MODE === 'prod' ? process.env.DB_HOST : process.env.LOCAL_DB_HOST,
-//     port: process.env.MODE === 'prod' ? parseInt(process.env.DB_PORT as string) : parseInt(process.env.LOCAL_DB_PORT as string),
-//     user: 'postgres',
-//     password: () => process.env.LOCAL_DB_PASSWORD!,
-//     database: process.env.MODE === 'prod' ? process.env.DB_NAME : process.env.LOCAL_DB_NAME,
-//     idleTimeoutMillis: 3000,
-//     connectionTimeoutMillis: 20000,
-//     max: 20000,
-//     allowExitOnIdle: true,
-// });
 
 pool.connect((err, client, release) => {
     if (err) {
