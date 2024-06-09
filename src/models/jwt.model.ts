fc599c511f3e35007cd7f5e125a24237c9b182e9
ioamra <iom.monchau@gmail.com>
2024-06-10 01:43:18 +0200

    useless model

--- src/models/jwt.model.ts
+++ src/models/jwt.model.ts
@@ -1,19 +1,3 @@
-import jwt from 'jsonwebtoken';
-
-export interface AccessToken {
-    privateKey: string,
-    publicKey: string,
-    expiresIn: number,
-    algorithm: jwt.Algorithm
-}
-
-export interface AccessToken {
-    privateKey: string,
-    publicKey: string,
-    expiresIn: number,
-    algorithm: jwt.Algorithm
-}
-
 export interface AccessTokenDecoded {
     id_users: number,
     role_users: string,
