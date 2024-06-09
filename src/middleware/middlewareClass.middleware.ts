58ab99ed8a804d3c339e3c2c04492ea25d6c200c
ioamra <iom.monchau@gmail.com>
2024-06-10 00:25:18 +0200

    routes admin

--- src/middleware/middlewareClass.middleware.ts
+++ src/middleware/middlewareClass.middleware.ts
@@ -33,4 +33,39 @@ export class MiddlewareFunction {
         }
     };
 
+    static async checkIsAdmin(req: Request, res: Response, next: NextFunction){
+        const client = await pool.connect();
+        try {
+            const token = req.headers.authorization;
+            if (!token) {
+                throw ErrorCode.MISSING_HEADERS;
+            };
+
+            const decodedToken = AuthenticationService.decodeToken(token) as AccessTokenDecoded;
+            if (!decodedToken) {
+                throw ErrorCode.MISSING_HEADERS;
+            }
+
+            const currentTime = Math.floor(Date.now() / 1000);
+            if (decodedToken.exp < currentTime) {
+                throw 'expired token';
+            }
+
+            if (decodedToken.role_users != 'admin') {
+                throw 'not admin';
+            }
+            
+            const { rows } = await client.query(`SELECT role_users FROM users WHERE id_users = $1`, [decodedToken.id_users])
+            if (rows[0].role_users != 'admin') {
+                throw 'nice try but you are not admin';
+            }
+
+            return next();
+        } catch (err) {
+            console.error(err + ' : error middleware access API');
+            return res.status(401).send({
+                message: err + ' : error middleware access API'
+            });
+        }
+    };
 }
---
+++ src/routes/admin-routes/usersAdmin.routes.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+import { getAllUsers } from "../../controllers/admin-controllers/users-admin-controllers/getAllUsers.controller";
+
+export const adminRouter = express.Router();
+
+adminRouter.get('/getAll', async (req: Request, res: Response) => {
+    getAllUsers(res);
+});
